be68c0cdf9f1ca5e4f1eab5141eaa46bd8da60f9
RUReady2 <ruready566@gmail.com>
2016-05-19 22:56:35 +0800

    Add TestScript

---
+++ server/TestScript.js
@@ -0,0 +1,33 @@
+var testServer = require('./TestServer'),
+    pushCommand = testServer.pushCommand,
+    pushKill = testServer.pushKill,
+    userAmount = 1,
+    userCounter = 0;
+
+function connectedCallback(obj){
+  console.log('------Client ' + obj['clientID'] + ' connected------');
+ 
+  if(++userCounter === userAmount)
+    mainFunction();
+}
+
+function stdoutCallback(obj){
+  console.log('================stdout================');
+  console.log(obj);
+}
+
+function stderrCallback(obj){
+  console.log('================stderr================');
+  console.log(obj);
+}
+
+function commandExitCallback(obj){
+  console.log('================Exit ID=' + obj['clientID']+ ', cmdID=' + obj['commandID'] + '================');
+}
+
+function mainFunction(){
+  pushCommand('pull', '#1', 'netstat', ['tln']);
+}
+
+testServer.setTestServerCallback(connectedCallback, stdoutCallback, stderrCallback, commandExitCallback);
+testServer.startTestServer();
